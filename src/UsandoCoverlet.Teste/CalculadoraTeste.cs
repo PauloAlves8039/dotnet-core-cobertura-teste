a1bac760121c9835ffc7d24ff166a83532b853f6
PauloAlves8039 <pj38alves@gmail.com>
2021-04-19 15:08:32 -0300

    Adicionando projeto no repositório

---
+++ src/UsandoCoverlet.Teste/CalculadoraTeste.cs
@@ -0,0 +1,51 @@
+using System;
+using src.UsandoCoverlet.App.Classes;
+using Xunit;
+
+namespace UsandoCoverlet.Teste
+{
+    public class CalculadoraTeste
+    {
+        Calculadora calculadora = new Calculadora();
+
+        [Fact]
+        public void Somar_Deve_Retornar_Verdadeira_A_Soma()
+        {
+            var valorRecebidoSoma = calculadora.Somar(5, 2);
+
+            var valorEsperadoSoma = 7;
+
+            Assert.Equal(valorEsperadoSoma, valorRecebidoSoma);
+        }
+
+        [Fact]
+        public void Subtrair_Deve_Retornar_Verdadeira_A_Subtracao()
+        {
+            var valorRecebidoSubtracao = calculadora.Subtrair(4, 1);
+
+            var valorEsperadoSubtracao = 3;
+
+            Assert.Equal(valorEsperadoSubtracao, valorRecebidoSubtracao);
+        }
+
+        [Fact]
+        public void Multiplicar_Deve_Retornar_Verdadeira_A_Multiplicacao()
+        {
+            var valorRecebidoMultiplicacao = calculadora.Multiplicar(3, 2);
+
+            var valorEsperadoMultiplicacao = 6;
+
+            Assert.Equal(valorEsperadoMultiplicacao, valorRecebidoMultiplicacao);
+        }
+
+        [Fact]
+        public void Dividir_Deve_Retornar_Verdadeira_A_Divisao()
+        {
+            var valorRecebidoDivisao = calculadora.Dividir(10, 2);
+
+            var valorEsperadoDivisao = 5;
+
+            Assert.Equal(valorEsperadoDivisao, valorRecebidoDivisao);
+        }
+    }
+}
